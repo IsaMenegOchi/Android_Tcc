f3f94c037b5824f9f9445ddb15bc0c26124a4502
Fernanda Ferreira <79976892+fernandaferreiras@users.noreply.github.com>
2022-03-18 11:22:32 -0300

    .

---
+++ app/src/main/java/com/example/tcc_after/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tcc_after;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
