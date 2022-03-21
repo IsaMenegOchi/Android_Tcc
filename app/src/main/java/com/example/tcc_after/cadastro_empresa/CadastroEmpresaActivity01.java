2f283e0d20b58b76693dd5346e751a6223a0a0b8
Isabelle M. Ochini <isabelle.ochini@gmail.com>
2022-03-21 11:28:35 -0300

    Fazendo tela de cadastro1 e iniciando cadastro2

---
+++ app/src/main/java/com/example/tcc_after/cadastro_empresa/CadastroEmpresaActivity01.java
@@ -0,0 +1,16 @@
+package com.example.tcc_after.cadastro_empresa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tcc_after.R;
+
+public class CadastroEmpresaActivity01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_empresa01);
+    }
+}
