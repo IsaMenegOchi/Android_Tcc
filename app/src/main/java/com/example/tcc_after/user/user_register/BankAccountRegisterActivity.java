5fa431f60785c00fb279ccd222c1948b2d840d96
“IsaMenegOchi” <isabelle.ochini@gmail.com>
2022-04-04 09:52:50 -0300

    Adicionando telas

---
+++ app/src/main/java/com/example/tcc_after/user/user_register/BankAccountRegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.tcc_after.user.user_register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tcc_after.R;
+
+public class BankAccountRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bank_account_register);
+    }
+}
