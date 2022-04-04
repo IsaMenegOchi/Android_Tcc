fd825d8b4ddc437b051372d858e4ee3abe3df338
Isabelle M. Ochini <isabelle.ochini@gmail.com>
2022-04-04 11:27:30 -0300

    Tentado fazer barra inferior

--- app/src/main/java/com/example/tcc_after/MainActivity.java
+++ app/src/main/java/com/example/tcc_after/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.tcc_after;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.tcc_after._bottom_menu_activity.FirstFragment;
+import com.example.tcc_after._bottom_menu_activity.SecondFragment;
+import com.example.tcc_after._bottom_menu_activity.ThirdFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(MainActivity.this);
+        bottomNavigationView.setSelectedItemId(R.id.perfilMenu);
+
+    }
+    FirstFragment firstFragment = new FirstFragment();
+    SecondFragment secondFragment = new SecondFragment();
+    ThirdFragment thirdFragment = new ThirdFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.searchMenu:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, firstFragment).commit();
+                return true;
+
+            case R.id.ticktesMenu:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, secondFragment).commit();
+                return true;
+
+            case R.id.homeMenu:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, thirdFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
