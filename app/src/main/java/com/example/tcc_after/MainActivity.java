497f0f11ef4d57c79f5b2a798f61f592d97f905b
Isabelle M. Ochini <isabelle.ochini@gmail.com>
2022-04-05 10:44:49 -0300

    Reorganizando menu inferior

--- app/src/main/java/com/example/tcc_after/MainActivity.java
+++ app/src/main/java/com/example/tcc_after/MainActivity.java
@@ -6,46 +6,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import com.example.tcc_after._bottom_menu_activity.FirstFragment;
-import com.example.tcc_after._bottom_menu_activity.SecondFragment;
-import com.example.tcc_after._bottom_menu_activity.ThirdFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
-    private BottomNavigationView bottomNavigationView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        // Initialize and assign variable
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(MainActivity.this);
-        bottomNavigationView.setSelectedItemId(R.id.perfilMenu);
+        // Set Home selected
+        bottomNavigationView.setSelectedItemId(R.id.home);
 
-    }
-    FirstFragment firstFragment = new FirstFragment();
-    SecondFragment secondFragment = new SecondFragment();
-    ThirdFragment thirdFragment = new ThirdFragment();
+        // Perform item selected listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId())
+                {
+                    case R.id.searchMenu:
+                        startActivity(new Intent(getApplicationContext(),ChoosePerfil.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.ticketMenu:
+                        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.homeMenu:
 
-        switch (item.getItemId()) {
-            case R.id.searchMenu:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, firstFragment).commit();
-                return true;
+                        return true;
+                }
+                return false;
+            }
+        });
 
-            case R.id.ticktesMenu:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, secondFragment).commit();
-                return true;
-
-            case R.id.homeMenu:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, thirdFragment).commit();
-                return true;
-        }
-        return false;
     }
 }
