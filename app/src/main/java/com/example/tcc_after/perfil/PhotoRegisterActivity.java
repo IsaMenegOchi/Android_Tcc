3712bead42e1089d9b981bf0e7e45c1537c1e003
“IsaMenegOchi” <isabelle.ochini@gmail.com>
2022-04-11 07:37:08 -0300

    Adicionando tela de cadastro de foto

---
+++ app/src/main/java/com/example/tcc_after/perfil/PhotoRegisterActivity.java
@@ -0,0 +1,45 @@
+package com.example.tcc_after.perfil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.tcc_after.R;
+
+public class PhotoRegisterActivity extends AppCompatActivity {
+
+    private ImageView photoPerfil;
+    private ImageView photoCover;
+
+    private final int CODE_IMAGE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_register);
+
+        photoPerfil = findViewById(R.id.ivPhotoRegister_Perfil);
+        photoCover = findViewById(R.id.ivPhotoRegister_Cover);
+
+        photoPerfil.setOnClickListener( view -> {
+
+            photoCover.setVisibility(View.VISIBLE);
+
+            openGalery();});
+    }
+
+    public void openGalery(){
+
+        //criamos uma variavel com
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+
+        //qual o tipo de recurso que quero pegar
+        intent.setType("image/*");
+
+        //abrir a activity responsavel por exibir as imagens, na qual retornará o conteudo selecionado para o nosso app
+        this.startActivityForResult(Intent.createChooser(intent, "Escolha uma foto"), CODE_IMAGE);
+    }
+}
