494cef9cfdf6f755c20f282f5f32fd8f08ff974a
ledierguilherme <79976654+ledierguilherme@users.noreply.github.com>
2022-04-18 11:20:25 -0300

    add tela verificationSolicitation

---
+++ app/src/main/java/com/example/tcc_after/UI/adm/VerificationSolicitationActivity.java
@@ -0,0 +1,22 @@
+package com.example.tcc_after.UI.adm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tcc_after.R;
+
+public class VerificationSolicitationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification_solicitation);
+    }
+}
+
+
+
+// falta fazer tratamento do click "Empresas", "Artistas"
+
+
