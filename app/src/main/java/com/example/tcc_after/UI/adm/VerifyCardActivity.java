79b6118c447928198c590467fbdc567aad366657
ledierguilherme <79976654+ledierguilherme@users.noreply.github.com>
2022-04-19 11:07:40 -0300

    add tela verifyCard

---
+++ app/src/main/java/com/example/tcc_after/UI/adm/VerifyCardActivity.java
@@ -0,0 +1,16 @@
+package com.example.tcc_after.UI.adm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tcc_after.R;
+
+public class VerifyCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_card);
+    }
+}
