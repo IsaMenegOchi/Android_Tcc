cbdc73e8644bdd435dedf37ac24a0a567c2fbc87
Isabelle M. Ochini <isabelle.ochini@gmail.com>
2022-06-07 06:14:12 -0300

    Fazendo menu

--- app/src/main/java/com/example/tcc_after/UI/MainUserActivity.java
+++ app/src/main/java/com/example/tcc_after/UI/MainUserActivity.java
@@ -30,71 +30,71 @@ public class MainUserActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         navigationView = findViewById(R.id.user_bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(navListner);
 
-        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment fragment = null;
-
-
-                switch (item.getItemId()){
-                    case R.id.nav_search:
-                        fragment = new SearchFragment();
-                        break;
-
-                    case R.id.nav_ticket:
-
-                        fragment = new TicketsFragment();
-
-                        if (fragment.equals(TicketsFragment.class)){
-                            item.setIcon(R.drawable.ic_baseline_local_activity);
-                        }
-                        else {
-                            item.setIcon(R.drawable.ic_outline_local_activity);
-                        }
-                        break;
-
-                    case R.id.nav_home:
-
-                        fragment = new FeedFragment();
-
-                        if (fragment.equals(FeedFragment.class)){
-                            item.setIcon(R.drawable.ic_baseline_home);
-                        }
-                        else{
-                            item.setIcon(R.drawable.ic_outline_home);
-                        }
-
-                        break;
-
-//                    case R.id.nav_moments:
-//
-//                        fragment = new MomentsFragment();
-//
-//                        if (fragment.equals(MomentsFragment.class)){
-//                            item.setIcon(R.drawable.ic_baseline_video_library);
-//                        }
-//                        else{
-//                            item.setIcon(R.drawable.ic_outline_video_library);
-//                        }
-//
-//                        break;
-
-                    case R.id.nav_perfil:
-
-                        fragment = new PerfilUserFragment();
-
-
-                        break;
-                }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.body_container, fragment).commit();
-
-                return true;
-            }
-        });
-
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_container, new FeedFragment()).commit();
 
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navListner = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+
+
+            switch (item.getItemId()) {
+                case R.id.nav_search:
+                    fragment = new SearchFragment();
+                    break;
+
+                case R.id.nav_ticket:
+
+                    fragment = new TicketsFragment();
+
+                    if (fragment.equals(TicketsFragment.class)) {
+                        item.setIcon(R.drawable.ic_baseline_local_activity);
+                    } else {
+                        item.setIcon(R.drawable.ic_outline_local_activity);
+                    }
+                    break;
+
+                case R.id.nav_home:
+
+                    fragment = new FeedFragment();
+
+                    if (fragment.equals(FeedFragment.class)) {
+                        item.setIcon(R.drawable.ic_baseline_home);
+                    } else {
+                        item.setIcon(R.drawable.ic_outline_home);
+                    }
+
+                    break;
+
+                    case R.id.nav_moments:
+
+                        fragment = new MomentsFragment();
+
+                        if (fragment.equals(MomentsFragment.class)){
+                            item.setIcon(R.drawable.ic_baseline_video_library);
+                        }
+                        else{
+                            item.setIcon(R.drawable.ic_outline_video_library);
+                        }
+
+                        break;
+
+                case R.id.nav_perfil:
+
+                    fragment = new PerfilUserFragment();
+
+
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.body_container, fragment).commit();
+
+            return true;
+        }
+    };
 
 }
+
